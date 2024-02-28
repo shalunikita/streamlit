ab288d0c9bf7d503c92df44aadc7229e117d1d33
Shalu Kumari <shalu.kumari@abzooba.com>
2024-02-28 19:22:32 +0530

    Changed Heading

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 def add_numbers(num1, num2):
     return num1 + num2
  
-st.title("Addition of  Any Two Numbers")
+st.title("Addition of Two Numbers")
  
 num1 = st.number_input("Enter the first number:")
 num2 = st.number_input("Enter the second number:")
